1a024d57932b0b89f38b47950c5e211476008f72
MisterG <g.haegerbaeumer@gmx.de>
2024-08-27 15:53:28 +0200

    Added Minimal jump time

--- playerController.gd
+++ playerController.gd
@@ -5,27 +5,35 @@ extends CharacterBody2D
 @export var jump_force: float = 200.0
 @export var start_move_speed: float = 300.0
 @export var add_speed_per_second: float = 50.0
+@export var min_jump_time: float = 0.2
 
 var move_speed: float
+var jump_release_buffer: bool = false;
+var jump_timer: float = 0
 
 func _ready() -> void:
 	move_speed = start_move_speed
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' s the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	move_speed += delta * add_speed_per_second
+	jump_timer += delta
 	
 	velocity.y += delta * gravity
-	
 	velocity.x = move_speed
 	
 	if Input.is_action_just_pressed("Jump"):
 		if is_grounded():
 			velocity.y = -jump_force
+			jump_timer = 0
 	
-	if Input.is_action_just_released("Jump"):
+	if Input.is_action_just_released("Jump") || jump_release_buffer:
 		if velocity.y < 0:
-			velocity.y = 0
+			if jump_timer > min_jump_time:
+				velocity.y = 0
+				jump_release_buffer = false
+			else:
+				jump_release_buffer = true
 	
 	move_and_slide()
 
