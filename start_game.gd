2b23a2ebd35a5011f171f5286ffa52c7f4be5dc3
Devid Dondorf <96085754+Dyspor@users.noreply.github.com>
2024-08-29 14:58:58 +0200

    FIXES

--- start_game.gd
+++ start_game.gd
@@ -1,14 +1,23 @@
-extends Control
+extends Node2D
 
-
+# This function handles the key input, changing the scene if "Jump" is pressed
 func _unhandled_key_input(event):
-	if event.is_pressed():
-		get_tree().change_scene_to_file("res://game.tscn")
+	if Input.is_action_just_pressed("Jump"):
+		get_tree().change_scene("res://game.tscn")
 
+# This function is called when the scene is ready
 func _ready() -> void:
-	# Ensure the node exists and then update its text
+	# Access nodes
 	var score_label = $ScoreNumb
+	var button = $TouchScreenButton
+
+	# Update the score label text
 	if score_label:
-		score_label.text = str(int(Global.points))
+		score_label.text = str(int(Global.points)) + " m"
 	else:
 		print("ScoreNumb label not found")
+
+# This function is called when the TouchScreenButton is pressed
+func _on_touch_screen_button_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+	
